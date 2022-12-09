21129042ae4aed785e448f915184e6a065025605
Aadi9910 <informarcialaditya@gmail.com>
2022-12-09 23:01:34 +0530

    nice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
-import "./App.css";
-import Rest from "./component/Basics/rest.js";
-const App = () => {
+import React, { useState } from "react";
+function useInput(defaultValue) {
+  const [value, setValue] = useState(defaultValue);
+  function onChange(e) {
+    setValue(e.target.value);
+  }
+  return {
+    value,
+    onChange,
+  };
+}
+function App() {
+  const inputProps = useInput();
   return (
-    <>
-      <Rest />
-    </>
+    <div className="wow">
+      <input {...inputProps} placeholder="Type in here" />
+      <div>Value: {inputProps.value} </div>
+    </div>
   );
-};
+}
 export default App;
