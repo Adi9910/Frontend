aaee46461b2055df081943410aafd26a7b47b12b
Aadi9910 <informarcialaditya@gmail.com>
2022-12-19 19:34:00 +0530

    nice work

--- src/DemoInput.js
+++ src/DemoInput.js
@@ -1,9 +1,37 @@
 import React, { useState } from "react";
 
 function DemoInput() {
+  const [input, setInput] = useState({ fullName: "" });
+  const [tableData, setTableData] = useState([]);
+  const handleChange = (evnt) => {
+    const newInput = (data) => ({
+      ...data,
+      [evnt.target.name]: evnt.target.value,
+    });
+    setInput(newInput);
+  };
+  const handleClick = (evnt) => {
+    evnt.preventDefault();
+    const newData = (data) => ({
+      ...data,
+      input,
+    });
+    setTableData(newData);
+  };
   return (
     <>
-      <div>hello</div>
+      <input value={input.fullName} onChange={handleChange} name="fullName" />
+      <button type="submit" onClick={handleClick}>
+        Submit
+      </button>
+      <th>Name</th>
+      {tableData.map((abc) => {
+        return (
+          <tr>
+            <td>{abc.fullName}</td>
+          </tr>
+        );
+      })}
     </>
   );
 }
