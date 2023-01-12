348049f6472dfbc503ae3ba5b05f22ea3cfb1276
Aadi9910 <informarcialaditya@gmail.com>
2023-01-12 23:24:04 +0530

    stream

--- src/setting.js
+++ src/setting.js
@@ -1,8 +1,46 @@
-import Webcam from "react-webcam";
-export default function WebcamSnap() {
-  return (
-    <>
-      <Webcam />
-    </>
-  );
-}
+// import Webcam from "react-webcam";
+// export default function WebcamSnap() {
+//   return (
+//     <>
+//       <Webcam />
+//     </>
+//   );
+// }
+
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+import React from "react";
+export default class WebcamSnap extends React.Component {
+    constructor(props) {
+      super(props);
+      this.streamCamVideo= this.streamCamVideo.bind(this)
+    }
+    streamCamVideo() {
+      var constraints = { audio: true, video: { width: 1280, height: 720 } };
+      navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(mediaStream) {
+          var video = document.querySelector("video");
+  
+          video.srcObject = mediaStream;
+          video.onloadedmetadata = function(e) {
+            video.play();
+          };
+        })
+        .catch(function(err) {
+          console.log(err.name + ": " + err.message);
+        }); 
+    }
+    render() {
+      return (
+        <div>
+          <div id="container">
+            <video autoPlay={true} id="videoElement" controls></video>
+          </div>
+          <br/>
+          <button onClick={this.streamCamVideo}>Start streaming</button>
+        </div>
+      );
+    }
+  }
