ba42d4c3f71d137eb6bd1883de50286daa2d2f90
Aadi9910 <informarcialaditya@gmail.com>
2023-01-15 20:49:31 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import Contact from "./Comp/Contact";
+import Home from "./Comp/Home";
+import Login from "./Comp/Login";
+import BasicExample from "./Comp/Navbar";
 import WebcamSnap from "./setting";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <WebcamSnap />
-    </div>
+    <>
+      <BasicExample />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="Login" element={<Login />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
